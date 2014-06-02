447079a0be1f7e50d4322915267500fdf0e99d7a
aashouwang <shouwangzhejuan@yahoo.cn>
2014-06-02 09:57:24 +0800

    Initial Commit

---
+++ 自定义tabbar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  自定义tabbar
+//
+//  Created by tieshan on 14-6-2.
+//  Copyright (c) 2014年 铁山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 自定义tabbar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  自定义tabbar
+//
+//  Created by tieshan on 14-6-2.
+//  Copyright (c) 2014年 铁山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
